4e0c1e1bda9e0937b8211bbefd69f151cca620ed
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-22 09:21:34 -0300

    Added fastify, and add another configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,70 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+import { v4 as uuidv4 } from 'uuid';
+import multipart from '@fastify/multipart';
+
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './utils/exceptions/all-exceptions.filter';
+import { TransformInterceptor } from './utils/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      genReqId: () => uuidv4(),
+    }),
+  );
+
+  await app.register(multipart, {
+    limits: {
+      fileSize: 20 * 1024 * 1024 * 2,
+    },
+  });
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Webhook Platform API')
+    .setDescription('Documentação técnica da Webhook Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Application is running on: http://localhost:${port}/v1`);
+  console.log(`📄 Swagger documentation: http://localhost:${port}/api`);
 }
+
 bootstrap();
