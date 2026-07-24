6ee98b54a9d6a2f4893c8b2a475252363906e31e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-24 14:38:43 -0300

    Set up mapper to user and your dtos

--- src/modules/user/mapper/user.mapper.ts
+++ src/modules/user/mapper/user.mapper.ts
@@ -1,44 +1,46 @@
-// src/infra/database/mapper/user.mapper.ts
 import { users } from "src/infra/database/schema/user.schema";
 import { User } from "../entities/user.entity";
 import { UserStatus } from "src/common/enums/user/user-status.enum";
+import { UpdateUserDto } from "../dto/update-user.dto";
 
 type SchemaUser = typeof users.$inferSelect;
 
 export class UserMapper {
     
+    static merge(user: User, dto: UpdateUserDto): void {
+        const updatableFields = Object.fromEntries(
+            Object.entries(dto).filter(([_, value]) => value !== undefined)
+        );
+
+        delete updatableFields.password;
+
+        Object.assign(user, updatableFields);
+    }
+    
     static toDomain(raw: SchemaUser): User {
         
         const user = new User();
-
-        user.id = raw.id;
-        user.version = raw.version;
-        user.createdAt = raw.createdAt;
-        user.updatedAt = raw.updatedAt;
-        user.deletedAt = raw.deletedAt;
-
-        user.name = raw.name;
-        user.fullName = raw.fullName;
-        user.email = raw.email;
-        user.passwordHash = raw.passwordHash;
-        user.emailVerified = raw.emailVerified;
+        
+        Object.assign(user, raw);
+        
         user.status = raw.status as UserStatus; 
-        user.lastLoginAt = raw.lastLoginAt;
 
         return user;
     }
 
     static toPersistence(user: User) {
+        const { id, name, fullName, email, passwordHash, emailVerified, status, lastLoginAt, version } = user;
+        
         return {
-            id: user.id,
-            name: user.name,
-            fullName: user.fullName,
-            email: user.email,
-            passwordHash: user.passwordHash,
-            emailVerified: user.emailVerified,
-            status: user.status,
-            lastLoginAt: user.lastLoginAt,
-            version: user.version,
+            id,
+            name,
+            fullName,
+            email,
+            passwordHash,
+            emailVerified,
+            status,
+            lastLoginAt,
+            version,
         };
     }
 }
