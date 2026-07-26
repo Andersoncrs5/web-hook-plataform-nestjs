159b257f2a4ab533c453767b4a838a461078fde8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-26 11:51:46 -0300

    Added JwtStrategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import {Inject, Injectable, UnauthorizedException} from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Payload } from '../classes/payload.class';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @Inject(ConfigService)
+        private readonly configService: ConfigService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+            issuer: configService.getOrThrow<string>('ISSUER'),
+            audience: configService.getOrThrow<string>('AUDIENCE'),
+        });
+    }
+
+    async validate(payload: Payload): Promise<Payload> {
+        if (!payload.sub) {
+            throw new UnauthorizedException('Token invalid');
+        }
+
+        return payload;
+    }
+}
