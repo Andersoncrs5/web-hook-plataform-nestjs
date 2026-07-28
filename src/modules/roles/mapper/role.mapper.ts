0d8415ad7414ec3b21232eccb8d00761bbc676bd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-28 14:52:52 -0300

    Set up RoleMapper

---
+++ src/modules/roles/mapper/role.mapper.ts
@@ -0,0 +1,39 @@
+import { roles } from "src/infra/database/schema/roles.schemas";
+import { Role } from "../entities/role.entity";
+import { CreateRoleDto } from "../dto/create-role.dto";
+import { UpdateRoleDto } from "../dto/update-role.dto";
+
+type SchemaRole = typeof roles.$inferSelect;
+
+export class RoleMapper {
+
+    static merge(user: Role, dto: UpdateRoleDto): void {
+        const updatableFields = Object.fromEntries(
+            Object.entries(dto).filter(([_, value]) => value !== undefined)
+        );
+
+        Object.assign(user, updatableFields);
+    }
+
+    static toDomain(raw: SchemaRole): Role {
+        return raw;
+    }
+
+    static toRole(dto: CreateRoleDto): Role {
+        return {
+            ...dto
+        } as Role
+    }
+
+    static toPersistence(role: Role) {
+
+        return {
+            id: role.id,
+            name: role.name,
+            description: role.description,
+            isActive: role.isActive,
+            version: role.version,
+        };
+    }
+
+}
