7f92ff1d1206bc1bc386f751ad0421686402e48a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-02 16:41:44 -0300

    Added SecurityModule

---
+++ src/common/crypto/security.module.ts
@@ -0,0 +1,16 @@
+import { Global, Module } from "@nestjs/common";
+import { CryptoService } from "./crypto.service";
+import { PasswordService } from "./password.service";
+
+@Global()
+@Module({
+    providers: [
+        CryptoService,
+        PasswordService,
+    ],
+    exports: [
+        CryptoService,
+        PasswordService,
+    ],
+})
+export class SecurityModule {}
