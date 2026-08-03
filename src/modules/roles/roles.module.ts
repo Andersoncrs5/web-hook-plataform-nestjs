1bba90c2d02069f6fef7abded2067c3eb30845fa
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-03 16:54:52 -0300

    Added RolesModule

---
+++ src/modules/roles/roles.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { RolesController } from './controllers/roles.controller';
+import { CreateRoleUseCase } from './services/create-role/create-role.use-case.service';
+import { DeleteRoleByIdUseCase } from './services/delete-role/delete-role-by-id.use-case.service';
+import { CheckRoleExistsByNameUseCase } from './services/exists-name/check-role-exists-by-name.use-case.service';
+import { FindAllRoleUseCase } from './services/find-all/find-all-role.use-case.service';
+import { FindByIdRoleUseCase } from './services/find-id-role/find-by-id-role.use-case.service';
+import { UpdateRoleUseCase } from './services/update-role/update-role.use-case.service';
+import { FindRoleByIds } from './services/find-role-by-ids/find-role-by-ids.use-case.service';
+
+@Module({
+  controllers: [RolesController],
+  providers: [
+    CreateRoleUseCase,
+    DeleteRoleByIdUseCase,
+    CheckRoleExistsByNameUseCase,
+    FindAllRoleUseCase,
+    FindByIdRoleUseCase,
+    UpdateRoleUseCase,
+    FindRoleByIds
+  ],
+  exports: [
+    CreateRoleUseCase,
+    DeleteRoleByIdUseCase,
+    CheckRoleExistsByNameUseCase,
+    FindAllRoleUseCase,
+    FindByIdRoleUseCase,
+    UpdateRoleUseCase,
+    FindRoleByIds
+  ],
+})
+export class RolesModule {}
