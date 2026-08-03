71aaf6def3179317913d6513b589db8c08426669
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-03 14:29:24 -0300

    Configured UserRoleMapper

---
+++ src/modules/user-role/mapper/user-role.mapper.ts
@@ -0,0 +1,31 @@
+import { userRoles } from "src/infra/database/schema/user.roles.schemas";
+import { UserRole } from "../entities/user-role.entity";
+import { CreateUserRoleDto } from "../dto/create-user-role.dto"; 
+
+type SchemaUserRole = typeof userRoles.$inferSelect;
+
+export class UserRoleMapper {
+
+    static toDomain(raw: SchemaUserRole): UserRole {
+        return raw as UserRole;
+    }
+
+    static toUserRole(dto: CreateUserRoleDto): UserRole {
+        return {
+            ...dto
+        } as UserRole;
+    }
+
+    static toPersistence(userRole: UserRole) {
+        return {
+            id: userRole.id,
+            userId: userRole.userId,
+            roleId: userRole.roleId,
+            version: userRole.version,
+            createdAt: userRole.createdAt,
+            updatedAt: userRole.updatedAt,
+            deletedAt: userRole.deletedAt,
+        };
+    }
+
+}
