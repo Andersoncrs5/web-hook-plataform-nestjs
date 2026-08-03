5d13e765ecfe975f86008c2dbbf531c335ac916b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-03 14:35:58 -0300

    Added repository, test and interface to user role repository

---
+++ src/modules/user-role/repository/iuser-role.repository.ts
@@ -0,0 +1,19 @@
+import { Page, Pageable } from "src/common/page/page";
+import { UserRoleFilter } from "../dto/user-role-filter.dto";
+import { UserRole } from "../entities/user-role.entity";
+import { UserRoleSort } from "../dto/user-role-sort.dto";
+
+export abstract class IUserRoleRepository {
+    abstract findAll(filter: UserRoleFilter, pageable: Pageable<UserRoleSort>): Promise<Page<UserRole>>
+
+    abstract deleteById(id: string): Promise<boolean>;
+
+    abstract update(user: UserRole): Promise<UserRole>;
+
+    abstract findById(id: string): Promise<UserRole | null>;
+
+    abstract create(user: UserRole): Promise<UserRole>;
+
+    abstract existsByRoleIdAndUserId(roleId: string, userId: string): Promise<boolean>;
+
+}
---
+++ src/modules/user-role/repository/user-role.repository.ts
@@ -0,0 +1,163 @@
+import { Injectable } from "@nestjs/common";
+import { SQL, and, asc, count, desc, eq, gte, lte } from "drizzle-orm";
+
+import { DatabaseService } from "src/infra/database/database.service";
+
+import { IUserRoleRepository } from "./iuser-role.repository";
+import { UserRole } from "../entities/user-role.entity";
+
+import { Page, Pageable } from "src/common/page/page";
+import { UserRoleFilter } from "../dto/user-role-filter.dto";
+import { UserRoleSort } from "../dto/user-role-sort.dto";
+import { UserRoleMapper } from "../mapper/user-role.mapper";
+import { userRoles } from "src/infra/database/schema/user.roles.schemas";
+import { users } from "src/infra/database/schema/user.schema";
+import { roles } from "src/infra/database/schema/roles.schemas";
+
+@Injectable()
+export class UserRoleRepository implements IUserRoleRepository {
+    constructor(
+        private readonly database: DatabaseService,
+    ) {}
+
+    async existsByRoleIdAndUserId(roleId: string, userId: string): Promise<boolean> {
+        const [existingUserRole] = await this.database.connection
+            .select({ id: userRoles.id })
+            .from(userRoles)
+            .where(
+                and(
+                    eq(userRoles.roleId, roleId),
+                    eq(userRoles.userId, userId)
+                )
+            )
+            .limit(1);
+
+        return !!existingUserRole;
+    }
+
+    async findById(id: string): Promise<UserRole | null> {
+        const [userRole] = await this.database.connection
+            .select()
+            .from(userRoles)
+            .where(eq(userRoles.id, id));
+
+        return userRole ? UserRoleMapper.toDomain(userRole) : null;
+    }
+
+    async create(userRole: UserRole): Promise<UserRole> {
+        const [created] = await this.database.connection
+            .insert(userRoles)
+            .values(UserRoleMapper.toPersistence(userRole))
+            .returning();
+
+        return UserRoleMapper.toDomain(created);
+    }
+
+    async update(userRole: UserRole): Promise<UserRole> {
+        const [updated] = await this.database.connection
+            .update(userRoles)
+            .set({
+                ...UserRoleMapper.toPersistence(userRole),
+                version: userRole.version + 1,
+                updatedAt: new Date(),
+            })
+            .where(eq(userRoles.id, userRole.id))
+            .returning();
+
+        return UserRoleMapper.toDomain(updated);
+    }
+
+    async deleteById(id: string): Promise<boolean> {
+        const deleted = await this.database.connection
+            .delete(userRoles)
+            .where(eq(userRoles.id, id))
+            .returning({
+                id: userRoles.id,
+            });
+
+        return deleted.length > 0;
+    }
+
+    async findAll(
+        filter: UserRoleFilter,
+        pageable: Pageable<UserRoleSort>,
+    ): Promise<Page<UserRole>> {
+        const conditions: SQL[] = [];
+
+        if (filter.id) conditions.push(eq(userRoles.id, filter.id));
+        
+        if (filter.userId) conditions.push(eq(userRoles.userId, filter.userId));
+        
+        if (filter.roleId) conditions.push(eq(userRoles.roleId, filter.roleId));
+        
+        if (filter.version !== undefined) conditions.push(eq(userRoles.version, filter.version));
+        
+        const sortableColumns = {
+            id: userRoles.id,
+            userId: userRoles.userId,
+            roleId: userRoles.roleId,
+            createdAt: userRoles.createdAt,
+            updatedAt: userRoles.updatedAt,
+        } as const;
+
+        const sortColumn = sortableColumns[pageable.sortBy] ?? userRoles.createdAt;
+        const whereCondition = conditions.length ? and(...conditions) : undefined;
+
+        let query = this.database.connection
+            .select({
+                id: userRoles.id,
+                userId: userRoles.userId,
+                roleId: userRoles.roleId,
+                version: userRoles.version,
+                createdAt: userRoles.createdAt,
+                updatedAt: userRoles.updatedAt,
+                deletedAt: userRoles.deletedAt,
+            })
+            .from(userRoles)
+            .$dynamic();
+
+        if (filter.loadUser) {
+            query = query.leftJoin(users, eq(userRoles.userId, users.id));
+        }
+
+        if (filter.loadRole) {
+            query = query.leftJoin(roles, eq(userRoles.roleId, roles.id));
+        }
+
+        const rawResult = await query
+            .where(whereCondition)
+            .orderBy(
+                pageable.direction === "asc"? asc(sortColumn) : desc(sortColumn)
+            )
+            .limit(pageable.size)
+            .offset((pageable.page - 1) * pageable.size);
+
+        const [{ totalElements }] = await this.database.connection
+            .select({ totalElements: count() })
+            .from(userRoles)
+            .where(whereCondition);
+
+        const mappedContent = rawResult.map((row: any) => {
+            const baseData = row.user_roles ? row.user_roles : row;
+            
+            const userRole = UserRoleMapper.toDomain(baseData);
+
+            if (filter.loadUser && row.users) {
+                userRole.user = row.users; 
+            }
+
+            if (filter.loadRole && row.roles) {
+                userRole.role = row.roles;
+            }
+
+            return userRole;
+        });
+
+        return new Page(
+            mappedContent,
+            pageable.page,
+            pageable.size,
+            totalElements,
+        );
+    }
+}
