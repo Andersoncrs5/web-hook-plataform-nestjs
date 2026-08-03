fea33af47a1a4c5be8c863356f38c77d9b24e619
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-03 16:07:51 -0300

    Added new RefreshToken

---
+++ src/modules/auth/resfresh-token/entities/refresh-token.entity.ts
@@ -0,0 +1,16 @@
+import { BaseDto } from "src/common/base/dto/base-dto.base";
+import { BaseEntity } from "src/common/base/entity/base.entity.base";
+import { RefreshTokenStatus } from "src/common/enums/refresh-token/refresh-token-status.enum";
+import { refreshTokens } from "src/infra/database/schema/refresh.tokens.schema";
+
+
+export type NewRefreshToken = typeof refreshTokens.$inferInsert;
+
+export class RefreshToken extends BaseEntity {
+    status: RefreshTokenStatus;
+    userId: string;
+    tokenHash: string;
+    expiresAt: Date;
+    revokedAt: Date | null;
+    replacedByTokenId: string | null;
+}
