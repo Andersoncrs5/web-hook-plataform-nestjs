fb52bc1d17032d9da30e34afebd16ad8e1058979
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-03 15:54:06 -0300

    .

--- src/modules/auth/guards/auth-guards.guard.ts
+++ src/modules/auth/guards/auth-guards.guard.ts
@@ -7,7 +7,7 @@ import {ConfigService} from "@nestjs/config";
 @Injectable()
 export class JwtGuard implements CanActivate {
     constructor(
-        private jwtService: JwtService,
+        private readonly jwtService: JwtService,
         private readonly configService: ConfigService,
     ) {
     }
