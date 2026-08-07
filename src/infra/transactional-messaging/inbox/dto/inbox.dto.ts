4f81f19f8e946418d9ec4d778d589b9e7418a1d7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-07 13:53:55 -0300

    added inbox dto

---
+++ src/infra/transactional-messaging/inbox/dto/inbox.dto.ts
@@ -0,0 +1,10 @@
+import { BaseDto } from "src/common/base/dto/base-dto.base";
+import { InboxStatus } from "src/utils/enums/inbox-status.enum";
+
+export class InboxDto extends BaseDto {
+    source: string
+    messageId: string
+    payload: string
+    status: InboxStatus
+    processedAt: Date
+}
