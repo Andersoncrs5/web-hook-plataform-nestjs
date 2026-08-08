0592ca6f5fab674e1aac5ad16dd548a4a5161f75
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-08 14:51:47 -0300

    Added more validation

--- src/modules/user-role/entities/user-role.entity.ts
+++ src/modules/user-role/entities/user-role.entity.ts
@@ -1,5 +1,6 @@
+import { IsUUID } from "class-validator";
 import { BaseEntity } from "src/common/base/entity/base.entity.base";
-import { userRoles } from "src/infra/database/schema/user.roles.schemas";
+import { userRoles } from "src/infra/database/schema/user.roles.schema";
 import { Role } from "src/modules/roles/entities/role.entity";
 import { User } from "src/modules/user/entities/user.entity";
 
@@ -7,9 +8,11 @@ export type NewUserRole = typeof userRoles.$inferInsert
 
 export class UserRole extends BaseEntity {
         
+    @IsUUID('all', { message: 'User ID must be a valid UUID' })
     userId: string;
     user?: User
 
+    @IsUUID('all', { message: 'Role ID must be a valid UUID' })
     roleId: string;
     role?: Role
 }
