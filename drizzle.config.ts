388fe19bd4e62fd637953728db4a6a5dc81ba369
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-08 15:03:20 -0300

    change path of out

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/infra/database/schema/*',
-  out: './drizzle',
+  schema: './src/infra/database/schema/*.schema.ts',
+  out: './src/infra/database/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
