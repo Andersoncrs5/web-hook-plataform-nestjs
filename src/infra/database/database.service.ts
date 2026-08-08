3b77841770652d64b27f69197f82a883830fda22
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-08 15:03:55 -0300

    improvement in class DatabaseService

--- src/infra/database/database.service.ts
+++ src/infra/database/database.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { db } from './drizzle';
+import { createDatabase } from './drizzle';
 
 @Injectable()
 export class DatabaseService {
+  private readonly client;
+  private readonly database;
+
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const { client, db } = createDatabase(databaseUrl);
+
+    this.client = client;
+    this.database = db;
+  }
+
   get connection() {
-    return db;
+    return this.database;
   }
 }
