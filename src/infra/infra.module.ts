fb6630d226f329676cb40507890478d32e82a0a9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-08 14:59:36 -0300

    Added items to sections import and exports

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { RedisModule } from './redis/redis.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { TransactionalMessagingModule } from './transactional-messaging/transactional-messaging.module';
 
 @Module({
-  imports: [DatabaseModule, RedisModule, RabbitmqModule],
-  exports: [DatabaseModule, RedisModule, RabbitmqModule],
+  imports: [
+    DatabaseModule, RedisModule, RabbitmqModule, TransactionalMessagingModule
+  ],
+  exports: [
+    DatabaseModule, RedisModule, RabbitmqModule, TransactionalMessagingModule
+  ],
 
 })
 export class InfraModule {}
