386441e2f1f237f59d6f3c1e364bc7fd8e05180d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-08 15:04:48 -0300

    Added items to section exports and providers

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -6,20 +6,34 @@ import { CreateUserUseCase } from './services/create-user/create-user.use-case.s
 import { DeleteByIdUserUseCase } from './services/delete-user/delete-user-by-id.use-case.service';
 import { FindUserByEmailUseCase } from './services/find-email/find-user-email.use-case.service';
 import { FindUserByIdUserUseCase } from './services/find-by-id/find-by-id-user.use-case.service';
+import { UpdateUserUseCase } from './services/update-user/update-user.use-case.service';
+import { FindAllUserUseCase } from './services/find-all/find-all.use-case.service';
 
 @Module({
   controllers: [UserController],
   providers: [
+    UserRepository,
     CreateUserUseCase,
     DeleteByIdUserUseCase,
+    FindAllUserUseCase,
     FindUserByEmailUseCase,
+    UpdateUserUseCase,
     FindUserByIdUserUseCase,
     {
       provide: IUserRepository,
       useClass: UserRepository,
     },
   ],
-  exports: [IUserRepository],
+  exports: [
+    UserRepository,
+    IUserRepository,
+    CreateUserUseCase,
+    DeleteByIdUserUseCase,
+    FindAllUserUseCase,
+    FindUserByEmailUseCase,
+    UpdateUserUseCase,
+    FindUserByIdUserUseCase,
+  ],
 })
 export class UserModule {}
 
