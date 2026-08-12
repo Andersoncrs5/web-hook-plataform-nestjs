4f901af894034f6cfe6a5b8e9d0411bf524d5768
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-12 15:32:17 -0300

    Added  items to sections providers and exports

--- src/infra/transactional-messaging/transactional-messaging.module.ts
+++ src/infra/transactional-messaging/transactional-messaging.module.ts
@@ -1,6 +1,37 @@
 import { Module } from "@nestjs/common";
+import { CreateInboxUseCase } from "./inbox/services/create-inbox/creare-inbox.use-case.service";
+import { DeleteInboxByIdUseCase } from "./inbox/services/delete-inbox/delete-inbox-by-id.use-case.service";
+import { ExistsInboxByMessageIdAndSourceUseCase } from "./inbox/services/exists-by-message-id-and-source/exists-by-message-id-and-source.use-case.service";
+import { FindByMessageIdAndSourceUseCase } from "./inbox/services/find-by-message-id-and-source/find-by-message-id-and-source.service";
+import { UpdateInboxUseCase } from "./inbox/services/update-inbox/update-inbox.use-case.service";
+import { IInboxRepository } from "./inbox/repository/iinbox.repository";
+import { InboxRepository } from "./inbox/repository/inbox.repository";
 
 @Module({
-    
+    providers: [
+        InboxRepository,
+
+        {
+            provide: IInboxRepository,
+            useExisting: InboxRepository,
+        },
+
+        CreateInboxUseCase,
+        DeleteInboxByIdUseCase,
+        ExistsInboxByMessageIdAndSourceUseCase,
+        FindByMessageIdAndSourceUseCase,
+        UpdateInboxUseCase,
+    ],
+
+    exports: [
+        InboxRepository,
+        IInboxRepository,
+
+        CreateInboxUseCase,
+        DeleteInboxByIdUseCase,
+        ExistsInboxByMessageIdAndSourceUseCase,
+        FindByMessageIdAndSourceUseCase,
+        UpdateInboxUseCase,
+    ],
 })
 export class TransactionalMessagingModule {}
