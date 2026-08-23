c0dd8a761e7b0a6cd4f4d69cb22d9bc9ed84281d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-23 15:56:55 -0300

    Added guards

---
+++ src/common/guards/guards/role.guard.ts
@@ -0,0 +1,53 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    ForbiddenException,
+} from '@nestjs/common';
+
+import { Reflector } from '@nestjs/core';
+import {ROLES_KEY} from "../../../modules/auth/decorators/roles.decorator";
+import { Payload } from '../../../modules/auth/classes/payload.class';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(
+        private readonly reflector: Reflector,
+    ) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+            ROLES_KEY,
+            [
+                context.getHandler(),
+                context.getClass(),
+            ],
+        );
+
+        if (!requiredRoles) {
+            return true;
+        }
+
+        const request = context.switchToHttp().getRequest();
+
+        const user = request.user as Payload;
+
+        if (!user) {
+            throw new ForbiddenException();
+        }
+
+        const userRoles: string[] = user.roles
+
+        const allowed = requiredRoles.some(role =>
+            userRoles.includes(role),
+        );
+
+        if (!allowed) {
+            throw new ForbiddenException(
+                'You do not have permission',
+            );
+        }
+
+        return true;
+    }
+}
