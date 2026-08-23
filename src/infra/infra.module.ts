2e9d503b82b7a5673e903e318e90051626b2e858
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-23 20:54:41 -0300

    Added modules in imports, exports and providers

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -3,14 +3,28 @@ import { DatabaseModule } from './database/database.module';
 import { RedisModule } from './redis/redis.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 import { TransactionalMessagingModule } from './transactional-messaging/transactional-messaging.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { IdempotencyInterceptor } from './transactional-messaging/inbox/annotation/idempotency.interceptor';
 
 @Module({
   imports: [
-    DatabaseModule, RedisModule, RabbitmqModule, TransactionalMessagingModule
+    DatabaseModule, 
+    RedisModule, 
+    RabbitmqModule, 
+    TransactionalMessagingModule,
+    
   ],
   exports: [
-    DatabaseModule, RedisModule, RabbitmqModule, TransactionalMessagingModule
+    DatabaseModule, 
+    RedisModule, 
+    RabbitmqModule, 
+    TransactionalMessagingModule
   ],
-
+  providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: IdempotencyInterceptor,
+        },
+    ],
 })
 export class InfraModule {}
