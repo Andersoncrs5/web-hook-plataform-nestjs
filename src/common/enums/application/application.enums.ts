6525c36caeaa3aa1c1efd3dd5138cef8a4234cc8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-29 16:46:03 -0300

    Added enums to application

---
+++ src/common/enums/application/application.enums.ts
@@ -0,0 +1,19 @@
+export enum ApplicationTypeEnum {
+  WEB = 'web',
+  MOBILE = 'mobile',
+  SPA = 'spa',
+  M2M = 'm2m',
+}
+
+export enum ApplicationEnvironmentEnum {
+  DEV = 'dev',
+  STAGING = 'staging',
+  PROD = 'prod',
+}
+
+export enum ApplicationStatusEnum {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  PENDING = 'pending',
+  ARCHIVED = 'archived',
+}
