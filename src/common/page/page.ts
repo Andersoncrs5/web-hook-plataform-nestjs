55339cab7ca2284dc4d543588bb968797108c825
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-30 17:03:34 -0300

    Added validations

--- src/common/page/page.ts
+++ src/common/page/page.ts
@@ -1,28 +1,49 @@
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+
 export enum SortDirection {
-    ASC = "asc",
-    DESC = "desc",
+  ASC = 'asc',
+  DESC = 'desc',
 }
 
 export class Pageable<T> {
-    page = 1;
-    size = 30;
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
 
-    sortBy: T;
-    direction = SortDirection.DESC;
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  size = 30;
+
+  sortBy: T;
+
+  @IsOptional()
+  @IsEnum(SortDirection)
+  direction = SortDirection.DESC;
 }
 
 export class Page<T> {
+  constructor(
+    readonly content: T[],
+    readonly page: number,
+    readonly size: number,
+    readonly totalElements: number,
+  ) {}
 
-    constructor(
-        readonly content: T[],
-        readonly page: number,
-        readonly size: number,
-        readonly totalElements: number,
-    ) {}
-
-    get totalPages(): number {
-        return Math.ceil(this.totalElements / this.size);
-    }
-    
+  get totalPages(): number {
+    return Math.ceil(this.totalElements / this.size);
+  }
 }
 
+export function normalizePageable<T>(pageable: Pageable<T>, defaultSort: T): Required<Pageable<T>> {
+  return {
+    page: pageable.page ?? 1,
+    size: pageable.size ?? 30,
+    sortBy: pageable.sortBy ?? defaultSort,
+    direction: pageable.direction ?? SortDirection.DESC,
+  };
+}
