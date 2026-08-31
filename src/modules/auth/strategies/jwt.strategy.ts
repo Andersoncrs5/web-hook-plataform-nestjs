7bdbe21255a341c60f8c1423580425c72e7959b6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-31 15:45:23 -0300

    New env

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import {Inject, Injectable, UnauthorizedException} from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
@@ -6,24 +6,24 @@ import { Payload } from '../classes/payload.class';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        @Inject(ConfigService)
-        private readonly configService: ConfigService,
-    ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
-            issuer: configService.getOrThrow<string>('ISSUER'),
-            audience: configService.getOrThrow<string>('AUDIENCE'),
-        });
+  constructor(
+    @Inject(ConfigService)
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: configService.getOrThrow<boolean>('IGNORE_EXPIRATION'),
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+      issuer: configService.getOrThrow<string>('ISSUER'),
+      audience: configService.getOrThrow<string>('AUDIENCE'),
+    });
+  }
+
+  async validate(payload: Payload): Promise<Payload> {
+    if (!payload.sub) {
+      throw new UnauthorizedException('Token invalid');
     }
 
-    async validate(payload: Payload): Promise<Payload> {
-        if (!payload.sub) {
-            throw new UnauthorizedException('Token invalid');
-        }
-
-        return payload;
-    }
+    return payload;
+  }
 }
