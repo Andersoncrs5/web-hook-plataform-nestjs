a54ad8ab1d469fdd4184e75c39c6bb5f9aa3c2fd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-02 08:43:35 -0300

    New method gone

--- src/common/result/result.ts
+++ src/common/result/result.ts
@@ -1,138 +1,144 @@
 import { HttpStatus } from '@nestjs/common';
 
 export class Result<T> {
-    private constructor(
-        private readonly _value: T | null,
-        private readonly _isSuccess: boolean,
-        private readonly _errors: string[],
-        private readonly _status: HttpStatus,
-    ) {}
+  private constructor(
+    private readonly _value: T | null,
+    private readonly _isSuccess: boolean,
+    private readonly _errors: string[],
+    private readonly _status: HttpStatus,
+  ) {}
 
-    // =========================
-    // FACTORY METHODS
-    // =========================
+  // =========================
+  // FACTORY METHODS
+  // =========================
 
-    static success<T>(value: T, status: HttpStatus = HttpStatus.OK): Result<T> {
-        return new Result(value as T, true, [], status);
+  static success<T>(value: T, status: HttpStatus = HttpStatus.OK): Result<T> {
+    return new Result(value as T, true, [], status);
+  }
+
+  static ok<T>(value?: T): Result<T> {
+    return new Result(value ?? (null as unknown as T), true, [], HttpStatus.OK);
+  }
+
+  static created<T>(value: T): Result<T> {
+    return new Result(value, true, [], HttpStatus.CREATED);
+  }
+
+  static badRequest<T>(...errors: string[]): Result<T> {
+    return new Result<T>(null as T, false, errors, HttpStatus.BAD_REQUEST);
+  }
+
+  static gone<T>(...errors: string[]): Result<T> {
+    return new Result<T>(null as T, false, errors, HttpStatus.GONE);
+  }
+
+  static notFound<T>(...errors: string[]): Result<T> {
+    return new Result<any>(null as T, false, errors, HttpStatus.NOT_FOUND);
+  }
+
+  static conflict<T>(...errors: string[]): Result<T> {
+    return new Result<any>(null as T, false, errors, HttpStatus.CONFLICT);
+  }
+
+  static forb<T>(...errors: string[]): Result<T> {
+    return new Result<any>(null as T, false, errors, HttpStatus.FORBIDDEN);
+  }
+
+  static failure<T>(errors: string[], status: HttpStatus): Result<T> {
+    return new Result<any>(null as T, false, errors, status);
+  }
+
+  static internalServer<T>(errors: string): Result<T> {
+    return new Result<any>(null as T, false, [errors], HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  static unauthorized<T>(errors: string): Result<T> {
+    return new Result<any>(null as T, false, [errors], HttpStatus.UNAUTHORIZED);
+  }
+
+  // =========================
+  // GETTERS
+  // =========================
+
+  get value(): T {
+    if (!this._isSuccess) {
+      return null as T;
     }
-    
-    static ok<T>(value?: T): Result<T> {
-        return new Result(value ?? (null as unknown as T), true, [], HttpStatus.OK);
-    }
+    return this._value as T;
+  }
 
-    static created<T>(value: T): Result<T> {
-        return new Result(value, true, [], HttpStatus.CREATED);
-    }
+  get errors(): string[] {
+    return this._errors;
+  }
 
-    static badRequest<T>(...errors: string[]): Result<T> {
-        return new Result<T>(null as T, false, errors, HttpStatus.BAD_REQUEST);
-    }
+  get error(): string {
+    return this._errors[0];
+  }
 
-    static notFound<T>(...errors: string[]): Result<T> {
-        return new Result<any>(null as T, false, errors, HttpStatus.NOT_FOUND);
-    }
+  get status(): HttpStatus {
+    return this._status;
+  }
 
-    static conflict<T>(...errors: string[]): Result<T> {
-        return new Result<any>(null as T, false, errors, HttpStatus.CONFLICT);
-    }
+  get isSuccess(): boolean {
+    return this._isSuccess;
+  }
 
-    static forb<T>(...errors: string[]): Result<T> {
-        return new Result<any>(null as T, false, errors, HttpStatus.FORBIDDEN);
-    }
+  get isFailure(): boolean {
+    return !this._isSuccess;
+  }
 
-    static failure<T>(errors: string[], status: HttpStatus): Result<T> {
-        return new Result<any>(null as T, false, errors, status);
-    }
+  get statusCode(): number {
+    return this._status;
+  }
 
-    static internalServer<T>(errors: string): Result<T> {
-        return new Result<any>(null as T, false, [errors], HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  // =========================
+  // TRANSFORMS
+  // =========================
 
-
-    static unauthorized<T>(errors: string): Result<T> {
-        return new Result<any>(null as T, false, [errors], HttpStatus.UNAUTHORIZED);
-    }
-
-
-    // =========================
-    // GETTERS
-    // =========================
-
-    get value(): T {
-        if (!this._isSuccess) {
-            return null as T;
-        }
-        return this._value as T;
-    }
-
-    get errors(): string[] {
-        return this._errors;
-    }
-
-    get status(): HttpStatus {
-        return this._status;
-    }
-
-    get isSuccess(): boolean {
-        return this._isSuccess;
-    }
-
-    get isFailure(): boolean {
-        return !this._isSuccess;
-    }
-
-    get statusCode(): number {
-        return this._status;
-    }
-
-    // =========================
-    // TRANSFORMS
-    // =========================
-
-    map<U>(fn: (value: T) => U): Result<U> {
-        if (this.isFailure) {
-            return Result.failure(this._errors, this._status) as Result<U>;
-        }
-
-        const newValue = fn(this._value as T);
-
-        return Result.success(newValue, this._status);
-    }
-
-    // =========================
-    // SIDE EFFECTS
-    // =========================
-
-    ifSuccess(fn: (value: T) => void): Result<T> {
-        if (this.isSuccess) fn(this._value as T);
-        return this;
+  map<U>(fn: (value: T) => U): Result<U> {
+    if (this.isFailure) {
+      return Result.failure(this._errors, this._status) as Result<U>;
     }
 
-    ifFailure(fn: (errors: string[]) => void): Result<T> {
-        if (this.isFailure) fn(this._errors);
-        return this;
-    }
+    const newValue = fn(this._value as T);
 
-    // =========================
-    // FALLBACKS
-    // =========================
+    return Result.success(newValue, this._status);
+  }
 
-    orElse(defaultValue: T): T {
-        return this.isSuccess ? (this._value as T) : defaultValue;
-    }
+  // =========================
+  // SIDE EFFECTS
+  // =========================
 
-    orElseGet(fn: () => T): T {
-        return this.isSuccess ? (this._value as T) : fn();
-    }
+  ifSuccess(fn: (value: T) => void): Result<T> {
+    if (this.isSuccess) fn(this._value as T);
+    return this;
+  }
 
-    orElseResult(fn: () => Result<T>): Result<T> {
-        return this.isSuccess ? this : fn();
-    }
+  ifFailure(fn: (errors: string[]) => void): Result<T> {
+    if (this.isFailure) fn(this._errors);
+    return this;
+  }
 
-    unwrapOr<R>(fn: (result: Result<T>) => R): T | R {
-        if (this.isFailure) {
-            return fn(this);
-        }
-        return this._value as T;
+  // =========================
+  // FALLBACKS
+  // =========================
+
+  orElse(defaultValue: T): T {
+    return this.isSuccess ? (this._value as T) : defaultValue;
+  }
+
+  orElseGet(fn: () => T): T {
+    return this.isSuccess ? (this._value as T) : fn();
+  }
+
+  orElseResult(fn: () => Result<T>): Result<T> {
+    return this.isSuccess ? this : fn();
+  }
+
+  unwrapOr<R>(fn: (result: Result<T>) => R): T | R {
+    if (this.isFailure) {
+      return fn(this);
     }
+    return this._value as T;
+  }
 }
