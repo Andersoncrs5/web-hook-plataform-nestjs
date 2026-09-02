cd97d06612cb856dab8bd11bc51a937828f6769d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-02 09:07:32 -0300

    Improvements in control of transactions

--- src/infra/interceptors/transaction/transaction.interceptor.ts
+++ src/infra/interceptors/transaction/transaction.interceptor.ts
@@ -1,27 +1,35 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-} from "@nestjs/common";
-import { from, lastValueFrom } from "rxjs";
-import { DatabaseService } from "src/infra/database/database.service";
-import { TransactionContextService } from "./transaction-context.service";
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable, lastValueFrom } from 'rxjs';
+import { FastifyReply } from 'fastify';
+import { transactionStorage } from './transaction.storage';
+import { DatabaseService } from 'src/infra/database/database.service';
 
 @Injectable()
 export class TransactionInterceptor implements NestInterceptor {
-  constructor(
-    private readonly database: DatabaseService,
-    private readonly txContext: TransactionContextService,
-  ) {}
+  constructor(private readonly databaseService: DatabaseService) {}
 
-  intercept(_: ExecutionContext, next: CallHandler) {
-    return from(
-      this.database.connection.transaction(async (tx: any) => {
-        return this.txContext.run(tx, async () => {
-          return lastValueFrom(next.handle());
-        });
-      }),
-    );
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+    return new Observable((subscriber) => {
+      this.databaseService.rawPool
+        .transaction(async (tx) => {
+          await transactionStorage.run(tx, async () => {
+            try {
+              const result = await lastValueFrom(next.handle());
+              const response = context.switchToHttp().getResponse<FastifyReply>();
+
+              if (response.statusCode >= 400 && response.statusCode <= 599) {
+                throw new Error(`Rollback forçado pelo status HTTP ${response.statusCode}`);
+              }
+
+              subscriber.next(result);
+              subscriber.complete();
+            } catch (error) {
+              subscriber.error(error);
+              throw error;
+            }
+          });
+        })
+        .catch(() => {});
+    });
   }
 }
