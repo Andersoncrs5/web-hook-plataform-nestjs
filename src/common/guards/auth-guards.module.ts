7be7dd19fed53dd54776d19c7237a228ed636811
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-02 14:44:25 -0300

    Added guard ApiKeyGuard

--- src/common/guards/auth-guards.module.ts
+++ src/common/guards/auth-guards.module.ts
@@ -1,12 +1,15 @@
 import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtGuard } from './guards/auth-guards.guard';
-import { RolesGuard } from './guards/role.guard';
+import { JwtGuard } from './guards/auth/auth-guards.guard';
+import { RolesGuard } from './guards/auth/role.guard';
+import { ApiKeyModule } from 'src/modules/api-key/api-key.module';
+import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 
 @Global()
 @Module({
   imports: [
+    ApiKeyModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -15,7 +18,7 @@ import { RolesGuard } from './guards/role.guard';
       }),
     }),
   ],
-  providers: [JwtGuard, RolesGuard],
-  exports: [JwtModule, JwtGuard, RolesGuard],
+  providers: [JwtGuard, RolesGuard, ApiKeyGuard],
+  exports: [JwtModule, JwtGuard, RolesGuard, ApiKeyGuard],
 })
 export class AuthGuardsModule {}
